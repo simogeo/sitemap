d1ddd952a96fb56688347f0496b52cb35baf9d36
simogeo <simon@linea21.com>
2012-12-20 12:26:29 +0100

    adding p tag

--- sample.php
+++ sample.php
@@ -7,10 +7,10 @@ $sitemap = new sitemap('http://www.mywebsite.com', './');
 $sitemap->generate();
 
 echo '<h2>Displaying entries number</h2>';
-echo $sitemap->getCounter(). ' entries have been added to the file.<br />';
+echo '<p>' . $sitemap->getCounter() . ' entries have been added to the file.</p>';
 
 echo '<h2>Accessing sitemap file</h2>';
-echo '<a href="./sitemap.xml">sitemap file</a>';
+echo '<p><a href="./sitemap.xml">sitemap file</a></p>';
 
 echo '<h2>Displaying log</h2>';
 echo $sitemap->getLog();
