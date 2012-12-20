019b93e8cacbff0dc50df97dd31025050f63c5a5
simogeo <simon@linea21.com>
2012-12-20 11:12:34 +0100

    closing h2

--- sample.php
+++ sample.php
@@ -6,10 +6,10 @@ include_once('class.sitemap.php');
 $sitemap = new sitemap('http://www.mywebsite.com', './');
 $sitemap->generate();
 
-echo '<h2>Displaying entries number<h2>';
+echo '<h2>Displaying entries number</h2>';
 echo $sitemap->getCounter(). ' entries have been added to the file.<br />';
 
-echo '<h2>Displaying log<h2>';
+echo '<h2>Displaying log</h2>';
 echo $sitemap->getLog();
 
 ?>
